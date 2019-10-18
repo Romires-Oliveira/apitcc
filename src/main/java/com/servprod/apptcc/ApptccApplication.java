648c52192b24d2fd3c7701fd84586db8eb59dcc7
Romirés Oliveira <romires2015@gmail.com>
2019-10-18 09:08:05 -0300

    Criação do Projeto

---
+++ src/main/java/com/servprod/apptcc/ApptccApplication.java
@@ -0,0 +1,13 @@
+package com.servprod.apptcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptccApplication.class, args);
+	}
+
+}
